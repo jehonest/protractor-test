5162093a075332af3c0506aa6be6c11ba9e764bd
Jenna Engebretson <jengebretson@thehonestcompany.com>
2017-06-26 15:09:25 -0700

    step 2.0

--- spec.js
+++ spec.js
@@ -1,33 +1,38 @@
-// spec.js - interacting w/ elements
+// spec.js - writing multiple scenarios
 
-/* Uses global element and by (created by Protractor)
- 		- element function: used for finding HTML elements on your webpage;
-			returns an ElementFinder object, which can be used to interact w/ the element
-			or get info from it; 
-			- sendKeys: type into <input>s
-			- click: click a button
-			- getText: return content of an element
-
-		- element(Locator) : Locator parameter describes how to find an element
-		- by object creates Locators
-			- by.model ...
-			- by.id ...
-			- by.binding ...
-			
-Run tests w/ 'protractor conf.js'
-*/
+// beforeEach function: runs before every it block
+// Store ElementFinders for input in reusable variables
 
 describe('Protractor Demo App', function() {
-  it('should add one and two', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  });
 
-    element(by.id('gobutton')).click();
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
 
-//     expect(element(by.binding('latest')).getText()).
-//         toEqual('5'); // This is wrong!
-    expect(element(by.binding('latest')).getText()).
-        toEqual('3'); // This is right!
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    // Fill this in.
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
   });
 });

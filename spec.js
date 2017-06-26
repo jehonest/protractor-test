0af32230ababf4562ee329ac931c30816626f860
Jenna Engebretson <jengebretson@thehonestcompany.com>
2017-06-26 15:21:15 -0700

    step 4.0

--- spec.js
+++ spec.js
@@ -1,38 +1,40 @@
-// spec.js - writing multiple scenarios
+// spec.js - lists of elements
 
-// beforeEach function: runs before every it block
-// Store ElementFinders for input in reusable variables
+/*
+
+- element.all: returns an ElementArrayFinder
+- Helper function, add
+- Added the variable history
+- Assert history has the expected length using the count method
+
+*/
 
 describe('Protractor Demo App', function() {
   var firstNumber = element(by.model('first'));
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
 
   beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
 
-  it('should add one and two', function() {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
+    expect(history.count()).toEqual(2);
 
-    goButton.click();
+    add(5, 6);
 
-    expect(latestResult.getText()).toEqual('3');
-  });
-
-  it('should add four and six', function() {
-    // Fill this in.
-    firstNumber.sendKeys(4);
-    secondNumber.sendKeys(6);
-
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('10');
+    //expect(history.count()).toEqual(0); // This is wrong!
+    expect(history.count()).toEqual(3); // This is right!
   });
 });

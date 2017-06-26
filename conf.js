5a9d3bf6b4f36368026aac2d51a00f900d9e28bc
Jenna Engebretson <jengebretson@thehonestcompany.com>
2017-06-26 15:13:18 -0700

    step 3.0

--- conf.js
+++ conf.js
@@ -1,13 +1,11 @@
 // conf.js
-
-// Configuration tells Protractor where your test files (specs) are,
-// and where to talk to your Selenium Server (seleniumAddress)
-// Specifies that we will be using Jasmine for the test framework; 
-// Defaults for all other configuration (Chrome is default browser)
-
-//run test with 'protractor conf.js'
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
+    browserName: 'chrome'
+  }]
 }
